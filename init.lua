ce361beeed77bd599e6e7be28da8c97b82d773fd
Balaji Yaswanth Vankala <balajiyaswanth.v@gmail.com>
2022-09-30 12:07:14 +0530

    added bufferline plugin

--- init.lua
+++ init.lua
@@ -1,15 +1,9 @@
 vim.o.number = true
 vim.bo.expandtab = true
-vim.bo.shiftwidth = 4
-vim.bo.softtabstop = 4
+vim.bo.shiftwidth = 2
+vim.bo.softtabstop = 2
 
 
 require('packer-plugin')
-
--- local keymap = vim.api.nvim_set_keymap
-
--- keymap('n', '<C-t>', ':NvimTreeToggle', { silent = true})
-
--- vim.api.nvim_set_keymap('n', '<C-t>', ':NvimTreeOpen<CR>', { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<C-x>', ':NvimTreeClose<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-t>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+require "plugin.bufferline-plugin"
+require "user.keymaps"
